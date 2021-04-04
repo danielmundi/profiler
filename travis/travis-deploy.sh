cb25b42cfd044063711993d42a08175fb7f39519
Daniel Finimundi <daniel@finimundi.com>
2021-04-04 11:45:08 -0300

    Fix deploy command

--- travis/travis-deploy.sh
+++ travis/travis-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEB_NAME=$(find ../ -name "*.deb")
+export DEB_NAME=$(find ./ -name "*.deb")
 
 echo "Deploying package: ${DEB_NAME}"
-curl -F package:${DEB_NAME} https://${FURYGEM_TOKEN}@push.fury.io/${FURYGEM_NAME}
+curl -F package=@${DEB_NAME} https://${FURYGEM_TOKEN}@push.fury.io/${FURYGEM_NAME}
