18f5797bce567c24d0021c536a678d44559c5fb7
Daniel Finimundi <daniel@finimundi.com>
2021-04-04 09:59:31 -0300

    Deploy deb branch

--- .travis.build.sh
+++ .travis.build.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+echo "Create chroot"
 sudo sbuild-createchroot --arch=${DEB_ARCH} ${DEB_DISTRO} \
     /srv/chroot/${DEB_DISTRO}-${DEB_ARCH}-sbuild http://deb.debian.org/debian
 
+echo "Build dsc"
 res=$(dpkg-source -b ./)
 
 dsc_file=$(echo "$res" | grep .dsc | grep -o '[^ ]*$')
 
+echo "Build package"
 sudo sbuild --arch=${DEB_ARCH} -c ${DEB_DISTRO}-${DEB_ARCH}-sbuild \
     -d ${DEB_DISTRO} ../${dsc_file}
 
